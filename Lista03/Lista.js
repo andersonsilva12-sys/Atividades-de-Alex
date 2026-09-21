e52b6c71daad2ff2c429162ac34aaf47ab65bec0
Anderson <anderson.silva.12@academico.ifpb.edu.br>
2026-09-21 08:32:37 -0300

    atualizando

--- Lista03/Lista.js
+++ Lista03/Lista.js
@@ -18,9 +18,9 @@ function verificarNum() {
         resultado = "Número Positivo!";
     }
     if (Num > 0) {
-        if (Num % 2 == 0){  
+        if (Num % 2 == 0) {
             resultado += ` O número é ${Num} par!`
-        }   else{
+        } else {
             resultado += ` O número ${Num} é ímpar`;
         }
     }
@@ -35,35 +35,35 @@ function verificarNum() {
 
 
 
-    function operadorMatematico(){
-        let Num1 = Number(document.getElementById("num1").value);
-        let Num2 = Number(document.getElementById("num2").value);
-        let Operacao = document.getElementById("operacao").value;
+function operadorMatematico() {
+    let Num1 = Number(document.getElementById("num1").value);
+    let Num2 = Number(document.getElementById("num2").value);
+    let Operacao = document.getElementById("operacao").value;
 
-        if (Operacao == "/" && Num1 == 0 || Num2 == 0) {
-            document.getElementById("result2").innerHTML = "Não é possível dividir por zero!";
-            return;
-        }
-
-        if (Operacao == "+"){
-            som = Num1 + Num2;
-            resultado = (`A soma dos dois número é: ${som}`);
-        } else if (Operacao == "-") {
-            sub = Num1 - Num2;
-            resultado = (`A subtração dos dois número é: ${sub}`);
-        } else if (Operacao == "*") {
-            mul = Num1 * Num2;
-            resultado = (`A multiplicação dos dois número é: ${mul}`);
-        } else if (Operacao == "/") {
-            div = Num1 / Num2;
-            resultado = (`A divisão dos dois número é: ${div}`);
-        }
-        else {
-            resultado = "Operação inválida. "
-        }
-
-        document.getElementById("result2").innerHTML = resultado;
+    if (Operacao == "/" && Num1 == 0 || Num2 == 0) {
+        document.getElementById("result2").innerHTML = "Não é possível dividir por zero!";
+        return;
     }
+
+    if (Operacao == "+") {
+        som = Num1 + Num2;
+        resultado = (`A soma dos dois número é: ${som}`);
+    } else if (Operacao == "-") {
+        sub = Num1 - Num2;
+        resultado = (`A subtração dos dois número é: ${sub}`);
+    } else if (Operacao == "*") {
+        mul = Num1 * Num2;
+        resultado = (`A multiplicação dos dois número é: ${mul}`);
+    } else if (Operacao == "/") {
+        div = Num1 / Num2;
+        resultado = (`A divisão dos dois número é: ${div}`);
+    }
+    else {
+        resultado = "Operação inválida. "
+    }
+
+    document.getElementById("result2").innerHTML = resultado;
+}
 
 //Questão 03: Classificação de idade
 /* Escreva uma função para classificar uma pessoa de acordo com sua idade.
@@ -75,7 +75,7 @@ Também informe sua situação eleitoral: menor de 16 anos: "Não pode votar", 1
 
 Se a idade for inválida, não faça a classificação eleitoral. */
 
-function classificadorIdade(){
+function classificadorIdade() {
     let Idade = Number(document.getElementById("idade").value)
 
     if (Idade < 0) {
@@ -90,13 +90,13 @@ function classificadorIdade(){
         resultado = ("Idoso. ");
     }
 
-   if (Idade < 16) {
-    resultado += ("Ainda não pode votar!");
-   } else if (Idade < 18 || Idade > 69) {
-    resultado += ("Possui voto facultativo!");
-   } else if (Idade > 18 && Idade < 70) {
-    resultado = ("Possui voto obrigatório!")
-   }
+    if (Idade < 16) {
+        resultado += ("Ainda não pode votar!");
+    } else if (Idade < 18 || Idade > 69) {
+        resultado += ("Possui voto facultativo!");
+    } else if (Idade > 18 && Idade < 70) {
+        resultado = ("Possui voto obrigatório!")
+    }
     document.getElementById("result3").innerHTML = resultado;
 }
 
@@ -167,32 +167,32 @@ function tarifaEnergia() {
 
     if (Consumo < 0) {
         resultado = (`Consumo inválido!`)
-    }   else if (Consumo <= 100) {
+    } else if (Consumo <= 100) {
         taxa = Consumo * 0.50
         valorTotal = taxa + taxaFixa
         resultado = `Seu consumo foi de: ${Consumo} kWh.
                     O valor de consumo é R$ 0.50 por kWh. Você deve pagar um taxa fixa de R$ 15.00. 
                     Valor total a pagar = R$ ${valorTotal}`;
-    }   else if (Consumo <= 200) {
+    } else if (Consumo <= 200) {
         taxa = Consumo * 0.75
         valorTotal = taxa + taxaFixa
         resultado = `Seu consumo foi de: ${Consumo} kWh.
                     O valor de consumo é R$ 0.75 por kWh. Você deve pagar um taxa fixa de R$ 15.00. 
                     Valor total a pagar = R$ ${valorTotal}`;
-    }   else if (Consumo <= 300) {
+    } else if (Consumo <= 300) {
         taxa = Consumo * 1.00
         valorTotal = taxa + taxaFixa
         resultado = `Seu consumo foi de: ${Consumo} kWh.
                     O valor de consumo é R$ 1.00 por kWh. Você deve pagar um taxa fixa de R$ 15.00. 
                     Valor total a pagar = R$ ${valorTotal}`;
-    }   else if (Consumo > 300) {
+    } else if (Consumo > 300) {
         taxa = Consumo * 1.25
         valorTotal = taxa + taxaFixa
         resultado = `Seu consumo foi de: ${Consumo} kWh.
                     O valor de consumo é R$ 1.25 por kWh. Você deve pagar um taxa fixa de R$ 15.00. 
                     Valor total a pagar = R$ ${valorTotal}`;
     }
-    
+
     document.getElementById("result5").textContent = resultado
 
 }
@@ -233,10 +233,10 @@ function calcularDesconto() {
             total = valorFinal * 0.85;
             resultado = (`Desconto de 15%. Valor total a ser pago: R$ ${total}`)
         }
-    } 
+    }
 
     if (Pagamento == "cartao") {
-          if (valorFinal <= 500) {
+        if (valorFinal <= 500) {
             total = valorFinal;
             resultado = (`Sem desconto. Valor total a ser pago: R$ ${total}`)
         }
@@ -272,16 +272,110 @@ function classificarTriangulos() {
     let somAC = ladoA + ladoC;
     let somBC = ladoB + ladoC;
 
-    if (somAB > ladoC && somAC > ladoB && somBC > ladoA){
-            if (ladoA == ladoB && ladoA == ladoC) {
-                document.getElementById("result7").innerHTML = "Esse é um triângulo equilátero!"        
-            } else if (ladoA != ladoB && ladoA !=ladoC) {
-                document.getElementById("result7").innerHTML = "Esse é um triângulo escaleno!"        
-            } else {
-                document.getElementById("result7").innerHTML = "Esse é um triângulo isóceles!"        
-            }
+    if (somAB > ladoC && somAC > ladoB && somBC > ladoA) {
+        if (ladoA == ladoB && ladoA == ladoC) {
+            document.getElementById("result7").innerHTML = "Esse é um triângulo equilátero!"
+        } else if (ladoA != ladoB && ladoA != ladoC) {
+            document.getElementById("result7").innerHTML = "Esse é um triângulo escaleno!"
+        } else {
+            document.getElementById("result7").innerHTML = "Esse é um triângulo isóceles!"
+        }
     } else {
         document.getElementById("result7").innerHTML = "Essas medidas não são de um triângulo!"
     }
     document.getElementById("result6").innerHTML = resultado
-}   
+}
+
+
+//Questão 8 — Validação de saque
+/* Escreva uma função para simular a validação de um saque em um caixa eletrônico.
+• Receba o saldo disponível e o valor desejado para saque.
+
+• Saldo menor que zero: “Saldo inválido”. Valor de saque menor ou igual a zero: “Valor
+de saque inválido”. Saque maior que o saldo: “Saldo insuficiente”.
+• O saque deve ser múltiplo de 10. Caso contrário, mostre “Valor deve ser múltiplo de 10”.
+• Se todas as condições forem satisfeitas, mostre “Saque autorizado” e o saldo restante.
+• Não é necessário calcular a quantidade de cédulas. */
+
+function validarSaque() {
+    let saldo = parseFloat(document.getElementById("saldo").value);
+    let saque = parseFloat(document.getElementById("saque").value);
+    let saldoFinal;
+
+    if (saldo < 0) {
+        document.getElementById("result8").innerHTML = ("Saldo inválido!");
+        return;
+    } else if (saque > saldo) {
+        document.getElementById("result8").innerHTML = ("Saldo insuficiente!");
+    } else if (saque % 10 != 0) {
+        document.getElementById("result8").innerHTML = ("Valor de saque deve ser multiplo de 10!");
+    } else {
+        saldoFinal = saldo - saque;
+        document.getElementById("result8").innerHTML = (`Saque efetuado com sucesso! Seu saldo agora é: ${saldoFinal}`)
+    }
+}
+
+
+//Questão 09: Imposto de renda simplificado
+/* • Escreva uma função para calcular um imposto simplificado com base no salário bruto.
+• Até R$ 2.000,00: isento; acima de R$ 2.000,00 até R$ 3.500,00: 7,5%; acima de R$ 3.500,00
+até R$
+5.000,00: 15%; acima de R$ 5.000,00: 22,5%.
+• Salário menor ou igual a zero deve ser considerado inválido.
+• Calcule o imposto sobre o salário bruto inteiro.
+• Mostre salário bruto, alíquota aplicada, valor do imposto e salário após o imposto. */
+
+
+function calcularImposto() {
+    let saldoBruto = parseFloat(document.getElementById("saldoBruto").value);
+    let saldoLiquido;
+    let resultado;
+    let imposto;
+
+    if (saldoBruto <= 0) {
+        resultado = "Saldo inválido";
+
+    } else if (saldoBruto <= 2000) {
+        saldoLiquido = saldoBruto;
+        resultado = `Você está isento de impostos! Seu saldo líquido é de: R$ ${saldoLiquido}`;
+
+    } else if (saldoBruto <= 3500) {
+        imposto = saldoBruto * 0.075;
+        saldoLiquido = saldoBruto * 0.925;
+        resultado = `Você deve pagar um imposto de 7,5% sendo: R$ ${imposto}. Seu saldo líquido é: R$ ${saldoLiquido}`;
+
+    } else if (saldoBruto <= 5000) {
+        imposto = saldoBruto * 0.15;
+        saldoLiquido = saldoBruto * 0.85;
+        resultado = `Você deve pagar um imposto de 15% sendo: R$ ${imposto}. Seu saldo líquido é: R$ ${saldoLiquido}`;
+
+    } else {
+        imposto = saldoBruto * 0.225;
+        saldoLiquido = saldoBruto * 0.775;
+        resultado = `Você deve pagar um imposto de 22,5% sendo: R$ ${imposto}. Seu saldo líquido é: R$ ${saldoLiquido}`;
+    }
+
+    document.getElementById("result9").innerHTML = resultado;
+}
+
+//Questão 10: Análise de crédito
+/* Um influenciador digital recebe dinheiro pelas publicações que realiza nas redes sociais. O valor
+de cada publicação depende da quantidade de seguidores.
+Escreva uma função que calcule quanto o influenciador receberá por uma publicação.
+O valor da publicação será definido da seguinte forma:
+• Até 10.000 seguidores: R$ 100,00;
+• De 10.001 a 50.000 seguidores: R$ 500,00;
+• De 50.001 a 100.000 seguidores: R$ 1.000,00;
+• Acima de 100.000 seguidores: R$ 2.000,00.
+Além disso, o influenciador receberá um bônus de acordo com a quantidade de curtidas:
+• Menos de 1.000 curtidas: sem bônus;
+• De 1.000 a 4.999 curtidas: bônus de R$ 100,00;
+• 5.000 curtidas ou mais: bônus de R$ 300,00.
+A função deve receber a quantidade de seguidores e de curtidas, calcular o valor da publicação,
+o bônus e o total recebido. */
+
+function analiseCredito() {
+    let seguidores = parseFloat(document.getElementById("seguidores").value);
+
+    
+}
